1d0bf945d522874aa0fec9c07724e0905d060943
William Wolf <throughnothing@gmail.com>
2015-02-15 00:43:28 -0800

    On Page Logging

--- examples/wallet.js
+++ examples/wallet.js
@@ -6,19 +6,28 @@ var Trezor         = require('../index'),
 
 var trezor = new TrezorDevice();
 
+var logBox = document.querySelector("#console");
+function log() {
+  var msg = '';
+  for(var i in arguments) {
+    msg += ' ' + arguments[i];
+  }
+  logBox.innerHTML = msg + '<br/>' + logBox.innerHTML; 
+}
+
 function onError(err) {
-  console.log('Error:', err);
+  log('Error:', err);
   trezor.disconnect().on('disconnect', function(){
-    console.log('Trezor disconnected.');
+    log('Trezor disconnected.');
   });
 }
 
 function onConnect() {
-  console.log('Trezor Connected.  DeviceID:', trezor.features.device_id);
+  log('Trezor Connected.  DeviceID:', trezor.features.device_id);
 }
 
 function onMessagePublicKey(message) {
-  console.log('Got Master Public Key:', message.decode().xpub);
+  log('Got Master Public Key:', message.decode().xpub);
 }
 
 function onPassphraseRequest(message) {
@@ -29,7 +38,7 @@ function onPassphraseRequest(message) {
 trezor.on('error', onError);
 trezor.on('connect', onConnect);
 trezor.on('message', function(message) {
-  console.log('message type, method:', message.type, message.getMethod());
+  log('message type, method:', message.type, message.getMethod());
   switch (message.type) {
     case TrezorMessages.MessageType.MessageType_PublicKey:
       onMessagePublicKey(message);
@@ -38,7 +47,7 @@ trezor.on('message', function(message) {
       onPassphraseRequest(message);
       break;
     default:
-      console.log('Unhandled Message Type:', message.getMethod());
+      log('Unhandled Message Type:', message.getMethod());
       break;
   }
 });
@@ -56,10 +65,10 @@ function getPublicKey() {
 function sendPassphraseAck(passphrase) {
   var password = document.querySelector("#password").value;
   if(!password) {
-    console.log("Password needed!");
+    log("Password needed!");
     return;
   }
-  console.log('Using password:', password);
+  log('Using password:', password);
   trezor.send('PassphraseAck', password);
   document.querySelector('#password-span').style.display = "none";
 }
