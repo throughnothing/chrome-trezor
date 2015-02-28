f898adb5ae8c924afb9575a7ec0121f69b7b90f7
William Wolf <throughnothing@gmail.com>
2015-02-27 20:37:49 -0800

    Work around usb.node issues in the browser + cleanup

--- scripts/node.js
+++ scripts/node.js
@@ -1,6 +1,6 @@
 'use strict';
 var trezor = require('..');
-var NodeUSB = trezor.usb.Node;
+var NodeUSB = require('../lib/usb/node.js');
 
 var t = new trezor.Device({ usb: NodeUSB });
 t.on('connect', function() {
